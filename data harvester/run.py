571c72fc3ec9786b2649d10da664c34f62494151
FANGFEI ZHENG <fangfeiz@student.unimelb.edu.au>
2020-05-21 21:57:18 +0800

    fix parameter

--- data harvester/run.py
+++ data harvester/run.py
@@ -7,9 +7,11 @@ Created on Thu May 21 14:11:18 2020
 import time
 import twitter_search
 import tweepy_streamer
+import sys
 
 def run(ip):
     server_path = 'http://admin:12345@'+ip+':5984/'
+    #server_path = 'http://admin:12345@localhost:5984/'
     tweepy_streamer.run(server_path)
     time.sleep(200)
     twitter_search.run(server_path)
@@ -17,11 +19,9 @@ def run(ip):
 
 if __name__ == "__main__":
 
-   # server_path = 'http://natlllu:12345@localhost:5984/'
-    
+    #server_path = 'http://natlllu:12345@localhost:5984/'    
     #server_path = 'http://' + username +':' + password +'@'+ip+':5984/'
-    ip=''
-    
+    ip = sys.argv[1]
     run(ip);
     
 
