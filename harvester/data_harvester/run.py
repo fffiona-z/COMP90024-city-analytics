3d0e15cab783dde602a94bed18d150c0830c326a
Ubuntu <ubuntu@docker.novalocal>
2020-05-22 09:19:45 +0000

    fix

--- harvester/data_harvester/run.py
+++ harvester/data_harvester/run.py
@@ -8,9 +8,9 @@ import twitter_search
 import tweepy_streamer
 import sys
 
-def run(ip):
-    server_path = 'http://admin:12345@'+ip+':5984/'
-    #server_path = 'http://admin:12345@localhost:5984/'
+def run():
+    #server_path = 'http://admin:12345@'+ip+':5984/'
+    server_path = 'http://admin:12345@localhost:5984/'
     tweepy_streamer.run(server_path)
     time.sleep(200)
     twitter_search.run(server_path)
@@ -20,8 +20,8 @@ if __name__ == "__main__":
 
     #server_path = 'http://natlllu:12345@localhost:5984/'    
     #server_path = 'http://' + username +':' + password +'@'+ip+':5984/'
-    ip = sys.argv[1]
-    run(ip);
+    #ip = sys.argv[1]
+    run();
     
 
 
---
+++ harvester/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/
+cd /data_harvester
+
+exec "$@"
+
