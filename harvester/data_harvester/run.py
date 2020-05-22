0183104171e24ed563b15f5ef8f227b8726ae4f8
Ubuntu <ubuntu@instance-0.novalocal>
2020-05-22 07:29:19 +0000

    fix bug

--- harvester/data_harvester/run.py
+++ harvester/data_harvester/run.py
@@ -2,7 +2,6 @@
 """
 Created on Thu May 21 14:11:18 2020
 
-@author: l
 """
 import time
 import twitter_search
@@ -22,10 +21,6 @@ if __name__ == "__main__":
     #server_path = 'http://natlllu:12345@localhost:5984/'    
     #server_path = 'http://' + username +':' + password +'@'+ip+':5984/'
     ip = sys.argv[1]
-<<<<<<< HEAD:data_harvester/run.py
-    
-=======
->>>>>>> ed6d0c1d092438280d2f933475961202d96c1ded:data harvester/run.py
     run(ip);
     
 
