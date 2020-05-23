8f8d5cee0beec8c442d0231fcabda4892bf38d23
Jingjiahui Lu <jingjiahui.lu@student.unimelb.edu.au>
2020-05-23 16:45:01 +1000

    update structures

--- harvester/data_harvester/run1.py
+++ harvester/data_harvester/run1.py
@@ -8,20 +8,19 @@ import twitter_search
 import tweepy_streamer
 import sys
 
-def run():
-    #server_path = 'http://admin:12345@'+ip+':5984/'
-    server_path = 'http://admin:12345@localhost:5984/'
+def run(server_path):
+  #  server_path = 'http://admin:12345@'+ip+':5984/'
+    #server_path = 'http://natlllu:12345@localhost:5984/'
     tweepy_streamer.run(server_path)
-    time.sleep(200)
-    twitter_search.run(server_path)
+
     
 
 if __name__ == "__main__":
 
-    #server_path = 'http://natlllu:12345@localhost:5984/'    
+    server_path = 'http://admin:12345@localhost:5984/'    
     #server_path = 'http://' + username +':' + password +'@'+ip+':5984/'
     #ip = sys.argv[1]
-    run();
+    run(server_path);
     
 
 
---
+++ harvester/data_harvester/run2.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 21 14:11:18 2020
+
+"""
+import time
+import twitter_search
+import tweepy_streamer
+import sys
+
+def run(server_path):
+    #server_path = 'http://admin:12345@'+ip+':5984/'
+#    server_path = 'http://admin:12345@localhost:5984/'
+    #tweepy_streamer.run(server_path)
+    twitter_search.run(server_path)
+    
+
+if __name__ == "__main__":
+
+    server_path = 'http://admin:12345@localhost:5984/'    
+    #server_path = 'http://' + username +':' + password +'@'+ip+':5984/'
+    #ip = sys.argv[1]
+    run(server_path);
+    
+
+
