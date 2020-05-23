57461ddfbba7b3cadfe518b72d8d9de7bfe6cb71
FANGFEI ZHENG <fangfeiz@student.unimelb.edu.au>
2020-05-23 17:55:59 +0800

    add template

--- webServer/app.py
+++ webServer/app.py
@@ -56,6 +56,7 @@ def search_from_db(view):
             tweets.append(dict(keys[1], row.value))
 
 
+
 if __name__ == '__main__':
     app.config.update(
         DEBUG = True,
@@ -66,4 +67,4 @@ if __name__ == '__main__':
     manager.setup(app)
     manager.add_viewdef([tweet_counts_view, aurin_counts_view])
     manager.sync(app)
-    app.run(host='0.0.0.0', port=5000, DEBUG=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
