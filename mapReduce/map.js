1c9ad1139b0c7ab4fb4bf545ab1830d0f7f4e8a8
Alex <pengnanz@student.unimelb.edu.au>
2020-05-23 22:41:56 +1000

    map reduce

--- mapReduce/map.js
+++ mapReduce/map.js
@@ -1,44 +1,64 @@
 function (doc) {
-
+  var crime_related = ["violence","fraud","genocide","addict","crook","derelict","felon",
+                       "hooker","hustler","junkie","miscreant","panhandler","suspect","thief","thugs","vandal",
+                       "conspiring","defrauding","fearless","greedy", "horrific", "killing", "maiming",
+                       "rampage", "senseless", "stalking", "terrorize", "threatening","Abuse", "Accessory",
+                       "accomplice", "accused", "accuser", "activists", "adversary","affect", "afis","aggravated", "assault",
+                        "alcohol", "arraignment", "arrest", "arsenal", "arson", "assailant", "assault", "attack", "autopsy",
+                       "belligerence", "blackmail", "bloodstain", "bombing", "brawl", "break-in", "breaking and entering", "Bribery",
+                       "Brutality", "Bullying", "Burglary", "Bystander","Cheat", "Civil", "Claim", "Coercion", "Collusion", "Combat",
+                       "Commission", "Controversial", "Conviction", "Cops", "Coroner", "Corruption", "theft", "Crime", "Criminal","Criminology", "Cuffs", "Custody",
+                       "Damage", "Danger", "Dangerous", "Dark side", "Deadly","Death","Deliberate", "Delinquency", "Detain","Dismember", "Disobedience",
+                        "Disorderly", "Dispatch", "Disruption","drug","Elusive", "Embezzle", "Emergency", "Enable","Escape", "Evil","Extort", "Extradition",
+                        "FBI", "Federal", "Felony", "Ferocity", "Fight", "fingerprint", "Firebombing", "flee", "Force", "Forensics", "Forgery",
+                        "Fraud", "furtive","gun","gang","Holster","Illegal", "Immoral", "Immunity", "Impeach", "Incarceration", "Incompetent", "Incriminating",
+                        "Indictment", "Injury", "Inmate", "Innocence", "Interrogate","Intruder", "Invasive", "Investigate","Jail","Jury","Juvenile","Kidnapping",
+                        "Kill", "Killer", "Kin","Leaks", "Lease", "Libel", "Liberty","Lie" , "Lowlife","Mace", "Malpractice", "Manacled", "Manslaughter", "Marshal",
+                        "Mayhem", "Metal", "Miscreant", "Misdemeanor", "Missing person", "money laundering", "Moratorium", "Motorist", "Murder",
+                        "Nuisance","Obey", "Obligation", "Offender", "Offense","Pedestrian","Penalize", "Penalty", "Perjury","Probation","Prohibit","Quake","Quarrel",
+                         "Radar", "Raid","Rape", "Refute", "Repeal", "Reported", "Resist","Restriction", "Revenge", "Riot", "Robbery", "Rogue", "Rough",
+                         "Sabotage", "Safeguard", "Sanction", "Scene", "Serial killer", "Seriousness", "Shackles", "Sheriff", "Shooting", "Shyster",
+                         "Slander", "Slashing", "Slaying", "Smuggling", "Sorrow", "Speculation", "Spying", "Squad", "Stabbing", "Stalking", "Statute",
+                         "Statute of limitation", "Stigma", "Stipulation", "Subdue", "Subpoena","Surveillance", "Suspect", "Sworn", "Terrorism",
+                         "Testify", "Testimony", "Theft", "Threatening", "Three-strikes law", "Thwart", "Tire-slashing", "Torture", "Tragedy",
+                         "Trauma", "Trooper", "Understaffed","Unlawful", "Vagrancy", "Vandalism", "Viable", "Vice", "Victim", "Victimize",
+                         "Violate", "Violation", "Violence","voyeurism", "Vulnerable", "Ward", "Warning", "Warped", "Warrant", "Weapon","Wiretap",
+                          "zealot"];
+  var cities = ["Darwin","Palmerston","Brisbane","Bundaberg" ,"Caboolture","Cairns","Caloundra","Gladstone","Gold Coast","Gympie","Hervey Bay","Ipswich"
+               ,"Logan","Mackay", "Maryborough", "Mount Isa","Rockhampton","Sunshine Coast","Toowoomba","Townsville", "Adelaide", "Mount BarkerMount",
+                "Gambier", "Murray Bridge", "Port Adelaide" ,"Port Augusta" ,"Port Pirie", "Port Lincoln", "Victor Harbor", "Whyalla","Hobart","Burnie",
+                ,"Devonport","Launceston", "Melbourne","Ararat","Bairnsdale","Benalla","Ballarat","Bendigo","Dandenong","Frankston","Geelong","Hamilton",
+                "Horsham", "Latrobe", "Melton","Mildura","Sale","Shepparton","Swan Hill","Wangaratta","Warrnambool","Wodonga","Perth", "Albany","Bunbury",
+                "Busselton","Fremantle","Geraldton","Joondalup","Kalgoorlie","Karratha","Mandurah","Rockingham","Sydney","Albury","Armidale","Bathurst",
+                "Blue Mountains","Broken Hill","Campbelltown","Cessnock","Dubbo","Goulburn","Grafton","Lithgow","Liverpool","Newcastle","Orange","Parramatta",
+                "Penrith","Queanbeyan","Tamworth","Wagga","Wollongong"]
+  var check = 0;
   if(doc.text != null){
-      if(doc.text.indexOf("covid") != -1)
-        emit(["COVID-19",doc.place.name],1);
-      else if(doc.text.indexOf("covid19") != -1)
-        emit(["COVID-19",doc.place.name],1);
-      else if(doc.text.indexOf("covid-19") != -1)
-        emit(["COVID-19",doc.place.name],1);
-      else if(doc.text.indexOf("COVID") != -1)
-        emit(["COVID-19",doc.place.name],1);
-      else if(doc.text.indexOf("COVID19") != -1)
-        emit(["COVID-19",doc.place.name],1);
-      else if(doc.text.indexOf("COVID-19") != -1)
-        emit(["COVID-19",doc.place.name],1);
-      else if(doc.text.indexOf("coronavirus") != -1)
-        emit(["COVID-19",doc.place.name],1);
-      else if(doc.text.indexOf("coronavirus19") != -1)
-        emit(["COVID-19",doc.place.name],1);
-      else if(doc.text.indexOf("coronavirus-19") != -1)
-        emit(["COVID-19",doc.place.name],1);
+      for(var i = 0; i < crime_related.length; i++){
+        if(doc.text.toLowerCase().indexOf(crime_related[i].toLowerCase()) != -1){
+            for(var j = 0; j < cities.length; j++){
+                if(doc.place.name == cities[j])
+                    check = 1;
+            }
+        }
+      }
+
+      if(check == 1)
+         emit(["Topic related to crime",doc.place.name],1);
+      check = 0;
   }
   else if(doc.tweet.text != null){
-    if( doc.tweet.text.indexOf("covid") != -1)
-       emit(["COVID-19",doc.tweet.place.name],1);
-    else if( doc.tweet.text.indexOf("covid-19") != -1)
-       emit(["COVID-19",doc.tweet.place.name],1);
-    else if( doc.tweet.text.indexOf("covid19") != -1)
-       emit(["COVID-19",doc.tweet.place.name],1);
-    else if( doc.tweet.text.indexOf("COVID") != -1)
-       emit(["COVID-19",doc.tweet.place.name],1);
-    else if( doc.tweet.text.indexOf("COVID19") != -1)
-       emit(["COVID-19",doc.tweet.place.name],1);
-    else if( doc.tweet.text.indexOf("COVID-19") != -1)
-       emit(["COVID-19",doc.tweet.place.name],1);
-    else if( doc.tweet.text.indexOf("coronavirus") != -1)
-       emit(["COVID-19",doc.tweet.place.name],1);
-    else if( doc.tweet.text.indexOf("coronavirus19") != -1)
-       emit(["COVID-19",doc.tweet.place.name],1);
-    else if( doc.tweet.text.indexOf("coronavirus-19") != -1)
-       emit(["COVID-19",doc.tweet.place.name],1);
-  }
+    for(var i = 0; i < crime_related.length; i++){
+          if( doc.tweet.text.toLowerCase().indexOf(crime_related[i].toLowerCase()) != -1){
+              for(var j = 0; j < cities.length; j++){
+                 if(doc.tweet.place.name == cities[j])
+                     check = 1;
+            }
+          }
 
+    }
+    if(check == 1)
+        emit(["Topic related to crime",doc.tweet.place.name],1);
+    check = 0;
+  }
 }
