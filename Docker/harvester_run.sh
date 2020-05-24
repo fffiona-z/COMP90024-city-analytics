cc5638cb8acb48fa4c10914fe06fafb372252583
FANGFEI ZHENG <fangfeiz@student.unimelb.edu.au>
2020-05-24 18:40:54 +0800

    del docker

--- Docker/harvester_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-docker login -u fei0990
-docker pull fei0990/comp90024_city_analysis:harvester
-docker tag fei0990/comp90024_city_analysis:harvester data_harvester:latest
-docker run --name harvester -d data_harvester
