70aed488abf9efada630a1f0d6ccf44a1a4b5fef
Ubuntu <ubuntu@instance-0.novalocal>
2020-05-25 06:18:07 +0000

    add run.sh

---
+++ harvester/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt install python-pip
+
+pip install -r requirements.txt
+
+echo 'start runing run1.py'
+
+nohup python ./data_harvester/run1.py >/home/ubuntu/output1 2>&1 &
+
+echo 'start runing run2.py'
+
+nohup python ./data_harvester/run2.py >/home/ubuntu/output2 2>&1 &
