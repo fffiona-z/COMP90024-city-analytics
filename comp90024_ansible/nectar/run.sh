f4e82f4494bfcf993935f492e4e6f6033d41ef91
xichen02 <42796270+xichen02@users.noreply.github.com>
2020-05-27 09:20:12 +0800

    Update run.sh

--- comp90024_ansible/nectar/run.sh
+++ comp90024_ansible/nectar/run.sh
@@ -4,4 +4,4 @@
 . ./openrc.sh; ansible-playbook --ask-become-pass apply_instance.yaml
 
 # setup couchdb cluster on database servers
-ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 couchdb_cluster_setup.yaml
+ansible-playbook -i inventory.ini -u ubuntu --key-file=./Group58 couchdb_cluster_setup.yml
